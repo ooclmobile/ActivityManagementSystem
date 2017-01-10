abf6520545dc87847f29c4d04a7a7fe116383584
Jack Yu <609107077@qq.com>
2017-01-10 10:15:52 +0800

    update

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'ActivityManagementSystem' do
+	pod 'MBProgressHUD', '~> 0.9.2'
+	pod 'Alamofire', '~> 3.1.5'
+end
---
+++ Pods/Target Support Files/Pods-ActivityManagementSystem/Pods-ActivityManagementSystem-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ActivityManagementSystem : NSObject
+@end
+@implementation PodsDummy_Pods_ActivityManagementSystem
+@end
